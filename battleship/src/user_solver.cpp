c688532c3f9775c6476c2fe1adab3e914cdc2a23
camron-coder <72827138+camron-coder@users.noreply.github.com>
2025-05-13 00:02:05 -0500

    Update user_solver.cpp

--- battleship/src/user_solver.cpp
+++ battleship/src/user_solver.cpp
@@ -2,7 +2,13 @@
 #include "../include/user_solver.h"
 
 void UserSolver::solve(Board &board) {
-    // Your Implementation goes here
+    const size_t x = board.getX();
+const size_t y = board.getY();
+for (size_t i = 0; i < x; ++i) {
+    for (size_t j = 0; j < y; ++j) {
+        guess(i, j, board);
+    }
+}
     ReturnType t = guess(1, 1, board);
     std::cout << t << std::endl;
 }
